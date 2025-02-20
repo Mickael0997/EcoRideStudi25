c9c774ef7934b9c4d6d323a2a6143af33fb381c3
Mickael0997 <158517216+Mickael0997@users.noreply.github.com>
2025-02-20 23:39:31 +0100

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mickael/ecoride',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f25bd9b236528af46bee506078ad7ea0a89cd775',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mickael/ecoride' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f25bd9b236528af46bee506078ad7ea0a89cd775',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
